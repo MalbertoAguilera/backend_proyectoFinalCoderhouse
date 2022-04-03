dae1efeb65c65ce2eae6538e93160980e90dae7c
Matias Aguilera <malbertoaguilera@outlook.es>
2022-04-03 15:30:37 -0300

    Sign Up View, CSRF Protection & User Model

--- version2/ecommerce/app.js
+++ version2/ecommerce/app.js
@@ -4,9 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const session = require('express-session')
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -22,10 +22,17 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//session
+app.use(session({
+      secret:"mi super secreto",
+      resave:false,
+      saveUninitialized:false
+}))
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
--- version2/ecommerce/routes/index.js
+++ version2/ecommerce/routes/index.js
@@ -2,13 +2,24 @@ var express = require("express");
 var router = express.Router();
 const Product = require("../models/schema/product");
 const arrayOfArray=require('../utils/arrayOfArray');
+const csurf =require('csurf');
+
+const csurfProtection= csurf();
+router.use(csurfProtection)
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   const products = await Product.find();
-  const productsPerRow = arrayOfArray(products);
-    res.render("shop", { products: productsPerRow, title:"Shopping Cart" });
+  const threeProductsPerPosition = arrayOfArray(products);
+    res.render("shop", { products: threeProductsPerPosition, title:"Shopping Cart" });
 });
+
+router.get("/user/signup", async (req, res, next) => {
+  res.render('signUp', {csurfToken: req.csrfToken(), title:"SignUp"});
+})
+router.post("/user/signup", async (req, res, next) => {
+  res.redirect('/');
+})
 
 
 module.exports = router;
